9b52ac8093a88f0c4f0e55908a57c00a79ac8035
Faique Ahmad Khan <fkhan.bese20seecs@seecs.edu.pk>
2022-11-22 23:56:46 +0500

    added styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,21 @@ export class AppComponent {
   public height!: number;
   public weight!:number;
   public bmi!:number;
+  public comment:string = "Check Your BMI.";
 
   bmiCal(){
 
       this.bmi = (this.weight/(this.height * this.height)) * 703;
       this.bmi = parseFloat(this.bmi.toFixed(1));
+      if (this.bmi < 18.5) {
+        this.comment = "You are underweight."
+      }else if (this.bmi < 24.9 && this.bmi > 18.5) {
+        this.comment = "You are healthy."
+      }else if (this.bmi < 29.9 && this.bmi > 24.9) {
+        this.comment = "You are overweight."
+      }else if (this.bmi > 29.9) {
+        this.comment = "You are obese."
+      }
     }
   }
 
